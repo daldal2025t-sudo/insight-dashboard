05bebe85358b59c8123e95ef0be1b058d4d86772
daldal2025 <daldal.2025.t@gmail.com>
2026-06-26 15:04:43 +0900

    fix: next.config.mjs에서 지원하지 않는 eslint 설정 제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 1. ESLint (코드 스타일 검사) 에러 무시
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   // 2. TypeScript (타입 문법 검사) 에러 무시
   typescript: {
     ignoreBuildErrors: true,
