3b81a513ecafa3a5a047ee931b5d90f52dd092c1
daldal2025 <daldal.2025.t@gmail.com>
2026-06-26 15:06:18 +0900

    fix: next.config.mjs에서 지원하지 않는 eslint 복구

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 1. ESLint (코드 스타일 검사) 에러 무시
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // 2. TypeScript (타입 문법 검사) 에러 무시
   typescript: {
     ignoreBuildErrors: true,
